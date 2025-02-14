bf2aff6c47733443ca9a4bb0a98e694a5a269246
Anshupriyaa <anshupriya05072004@gmail.com>
2025-02-14 22:53:28 +0530

    [Anshu priya] add:Unchecked Exception code added✨

---
+++ Day04/src/com/day04/exceptionhandling/UncheckedException.java
@@ -0,0 +1,28 @@
+package com.day04.exceptionhandling;
+
+import java.util.*;
+
+public class UncheckedException {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+
+            System.out.print("Enter the first number: ");
+            int num1 = scanner.nextInt();
+            System.out.print("Enter the second number: ");
+            int num2 = scanner.nextInt();
+            int result = num1 / num2;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            // Handle division by zero
+            System.out.println("Error: Division by zero is not allowed.");
+        } catch (InputMismatchException e) {
+            // Handle non-numeric input
+            System.out.println("Error: Please enter valid numeric values.");
+        } finally {
+
+            scanner.close();
+        }
+    }
+}
