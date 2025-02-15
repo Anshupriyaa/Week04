e987d57c8d151acb37483f5e740f98cec046fbc5
Anshupriyaa <anshupriya05072004@gmail.com>
2025-02-15 22:23:43 +0530

    [Anshu priya] add:Exception Handling code added✨

---
+++ Day05/src/main/java/com/tit/regexandjunit/junit/exceptionhandling/ExceptionHandling.java
@@ -0,0 +1,8 @@
+package com.tit.regexandjunit.junit.exceptionhandling;
+
+public class ExceptionHandling {
+    public int divide(int a , int b) throws ArithmeticException{
+        int result = a/b;
+        return result;
+    }
+}
