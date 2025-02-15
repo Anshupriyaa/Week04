1b2b5e823e5d8b6e451f57d60a933662221d2fc5
Anshupriyaa <anshupriya05072004@gmail.com>
2025-02-15 22:22:13 +0530

    [Anshu priya] add:Data base Connection code added✨

---
+++ Day05/src/main/java/com/tit/regexandjunit/junit/databaseconnection/DatabaseConnection.java
@@ -0,0 +1,18 @@
+package com.tit.regexandjunit.junit.databaseconnection;
+public class DatabaseConnection {
+    private boolean isConnected;
+
+    public void connect() {
+        isConnected = true;
+        System.out.println("Database connected");
+    }
+
+    public void disconnect() {
+        isConnected = false;
+        System.out.println("Database disconnected");
+    }
+
+    public boolean isConnected() {
+        return isConnected;
+    }
+}
